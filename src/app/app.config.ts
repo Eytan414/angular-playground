b242195178e2ee47789467b16d9ae87c58f3de06
eytan414 <eytanivri@gmail.com>
2024-07-17 21:33:10 +0300

    update ng core 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from "@angular/common/http";
+import {HttpClientModule, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import { routes } from './app.routes';
 import { ControllerService } from './services/controller.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
       provideRouter(routes),
       ControllerService,
-      importProvidersFrom(HttpClientModule),
+      provideHttpClient(withInterceptorsFromDi()),
       provideAnimationsAsync()  
     ]
 };
